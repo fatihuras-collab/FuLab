c47b0644bd50b5196f8baf6b0656fc8a6322c62e
fatihuras-collab <fatihsaman@gmail.com>
2026-09-19 21:38:52 +0300

    Add files via upload

---
+++ dist/config.js
@@ -0,0 +1,2 @@
+// Yalnızca doğrulanmış kurumsal bilgileri girin. Gizli anahtar yazmayın.
+window.FULAB_CONFIG = { email: 'fatih@fulabtr.net', phone: '+90 555 104 80 80', whatsapp: '905551048080' };
